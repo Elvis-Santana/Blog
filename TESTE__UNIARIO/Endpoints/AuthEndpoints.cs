b3f4c8015f9ff5e7afa9633a4d1a78728b406563
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-10-22 20:45:41 -0300

    [feat]:add AuthEndpoints e add rota de post login

---
+++ TESTE__UNIARIO/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using Application.Dtos.Models;
+using Application.IServices;
+namespace TESTE__UNIARIO.Endpoints;
+
+public static class AuthEndpoints
+{
+
+    public static WebApplication RouterAuthEndpoints(this WebApplication app)
+    {
+        const string nameGroup = "auth";
+        var auth = app.MapGroup($"/{nameGroup}");
+
+        auth.WithTags(nameGroup);
+
+
+        auth.MapPost("/",async (IAuthSevice authSevice, Login login) =>
+        {
+            return await authSevice.CriateToken(login);
+        });
+
+
+        return app;
+    }
+}
