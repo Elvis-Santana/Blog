21586f18a05be8815a87cc2f2daec39e29f31751
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-27 14:31:16 -0300

    feat :Add IFollowRepository

---
+++ Application/IRepository/IFollowRepository/IFollowRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IRepository.IFollowRepository;
+
+public interface IFollowRepository
+{
+    void CreateFollow(Follow follow);
+
+    void RemoveFollow(Follow follow);
+
+}
