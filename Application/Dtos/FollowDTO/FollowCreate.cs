8da6bc6e7f62f1c403795a6e7cc0ad973900fa5a
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-27 13:36:35 -0300

    feat : add FollowCreate e FollowRead

---
+++ Application/Dtos/FollowDTO/FollowCreate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.FollowDTO;
+
+public record FollowCreate(string FollowerId, string FollowingId);
+
+
---
+++ Application/Dtos/FollowDTO/FollowRead.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos.FollowDTO;
+
+public record FollowRead(string FollowerId, string FollowingId);
+
+
