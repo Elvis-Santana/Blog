a1ddb52ab82f8e9b8c7543227db28058d8ac1445
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-27 13:39:15 -0300

    test : add teste CreateFollow_WithValidParameters_SetsFollowerAndFollowingIds

---
+++ BlogTest/EntitiesTests/FollowTest.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogTest.EntitiesTest;
+
+public class FollowTest
+{
+
+
+    [Fact]
+    public void CreateFollow_WithValidParameters_SetsFollowerAndFollowingIds ()
+    {
+
+        string FollowerId = Guid.NewGuid().ToString();
+        string FollowingId = Guid.NewGuid().ToString(); 
+
+        Follow follow = Follow.CreateFollow(FollowerId, FollowingId);
+
+
+        follow.FollowerId.Should().Be(FollowerId);
+        follow.FollowingId.Should().Be(FollowingId);
+
+    }
+}
