7a85e09736a653170d57a1e81475310703945181
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-26 22:06:11 -0300

    feat : add entity de Fallow e Notification

---
+++ Domain/Entities/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Follow
+{
+    public string FollowerId { get; init; }
+    public string AuthorId { get; init; }  
+
+    protected Follow() { }
+
+    public Follow(string FollowerId,string AuthorId)
+    {
+        this.FollowerId = FollowerId;
+        this.AuthorId = AuthorId;
+    }
+}
---
+++ Domain/Entities/Notification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Notification
+{
+    public string Id { get; init; }
+    public string UserId { get; init; }
+    public string Messsage { get; init; }
+    public bool IsRead { get; private set; } = default(bool);
+
+
+    protected Notification() { }
+
+    private Notification(string UserId,string Messsage)
+    {
+        this.Id = Guid.NewGuid().ToString();
+        this.UserId = UserId;
+        this.Messsage = Messsage;
+    }
+
+    public void NotificationIsRead() => IsRead = true;
+
+    public static Notification Create(string UserId, string Messsage) => new(UserId,Messsage);
+
+}
