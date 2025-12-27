3f8316c003435c0e37d818875b6cf8bd19552619
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-26 22:10:43 -0300

    feat : add InfrastructureConection

---
+++ Infrastructure/InfrastructureConection.cs
@@ -0,0 +1,27 @@
+﻿using Application.IRepository.IAuthorRepository;
+using Application.IRepository.ICategoryRepository;
+using Application.IRepository.IPostRepository;
+using Application.IUnitOfWork;
+using Infrastructure.Db;
+using Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureConection
+{
+    public static IServiceCollection AddInfrastructure( this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<DbContextLite>(options =>
+            options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IAuthorRepository, AuthorRepository>();
+        services.AddScoped<IPostRepository, PostRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+
+        return services;
+    }
+}
