388038cb86b30bd87681b4f47609d386f48e1e76
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-26 22:17:48 -0300

    refactor : convergindo configuração em funções

--- BlogApi/Extensions/BuilderExtensions.cs
+++ BlogApi/Extensions/BuilderExtensions.cs
@@ -1,21 +1,5 @@
 ﻿using Application;
-using Application.Dtos.Models;
-using Application.IServices;
-using Application.Services.AuthorService;
-using Application.Services.AuthSevice;
-using Application.Services.CategoryService;
-using Application.Services.PostService;
-using Application.Validators.Validator.AuthorValidator;
-using Application.Validators.Validator.CategoryValidator;
-using Application.Validators.Validator.PostValidator;
-using Domain.IRepository.IAuthorRepository;
-using Domain.IRepository.ICategoryRepository;
-using Domain.IRepository.IPostRepository;
-using FluentValidation;
-using Infrastructure.Db;
-using Infrastructure.Repository;
-using Infrastructure.UnitOfWork;
-using Microsoft.EntityFrameworkCore;
+using Infrastructure;
 using Microsoft.OpenApi.Models;
 
 namespace TESTE__UNIARIO.Extensions;
@@ -24,86 +8,49 @@ public static class BuilderExtensions
 {
     public static IServiceCollection AddServices(this IServiceCollection services,IConfiguration builder)
     {
-        services.AddDbContext<DbContextLite>(options => options.UseSqlite(builder.GetConnectionString("DefaultConnection")));
-        //services.AddDbContext<DbContextLite>(options => options.UseSqlServer("postgresql://postgres:12345@localhost:5432/postgres"));
-
-
-        ScopedRepository(services);
-        ScopedService(services);
-        Validator(services);
-        ConfigSwagger(services);
-
-        return services;
-
-        static void Validator(IServiceCollection services)
+        return services
+        .AddInfrastructure(builder)
+        .AddApplication()
+        .AddSwaggerGen(options =>
         {
-            services.AddScoped<IValidator<AuthorCreateDTO>, AuthorCreateValidator>();
-            services.AddScoped<IValidator<CategoryCreateDTO>, CategoryCreateValidator>();
-            services.AddScoped<IValidator<CategoryUpdateDTO>, CategoryUpdateValidator>();
-            services.AddScoped<IValidator<PostCreateDTO>, PostCreateValidator>();
-            services.AddScoped<IValidator<PostUpdateDTO>, PostUpdateValidator>();
-        }
-
-        static void ScopedService(IServiceCollection services)
-        {
-            services.AddScoped<IAuthSevice, AuthSevice>();
-            services.AddScoped<ICategoryService, CategoryService>();
-            services.AddScoped<IServiceAuthor, AuthorService>();
-            services.AddScoped<IPostService, PostService>();
-        }
-
-        static void ScopedRepository(IServiceCollection services)
-        {
-            services.AddScoped<IAuthorRepository, AuthorRepository>();
-            services.AddScoped<IPostRepository, PostRepository>();
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-        }
-
-        static void ConfigSwagger(IServiceCollection services)
-        {
-            services.AddSwaggerGen(options =>
+            options.SwaggerDoc("v1", new OpenApiInfo
             {
-                options.SwaggerDoc("v1", new OpenApiInfo
+                Title = "Minha Minimal API",
+                Version = "v1",
+                Description = "Documentação automática com Swagger",
+                Contact = new OpenApiContact
                 {
-                    Title = "Minha Minimal API",
-                    Version = "v1",
-                    Description = "Documentação automática com Swagger",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Elvis",
-                        Email = "seuemail@exemplo.com"
-                    }
-                });
-
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "JWT",
-                });
-
-               options.AddSecurityRequirement(new OpenApiSecurityRequirement {
-
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[]{}
-                    }
-
-              });
+                    Name = "Elvis",
+                    Email = "seuemail@exemplo.com"
+                }
             });
-        }
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT",
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+
+            });
+        });   
     }
 
 
