9d846b516d7b28b62f236b3d8b7414e8a39ac079
Elvis Santana <elvissantanaguimaraes@gmail.com>
2025-12-27 14:43:25 -0300

    feat : add FollowRepository  e  implementandoIFollowRepository

---
+++ Infrastructure/Repository/FollowRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.IRepository.IFollowRepository;
+using Domain.Entities;
+using Infrastructure.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository;
+
+public class FollowRepository (DbContextLite dbContextLite) : IFollowRepository
+{
+    private readonly DbContextLite _dbContextLite = dbContextLite;
+    public void CreateFollow(Follow follow)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveFollow(Follow follow)
+    {
+        throw new NotImplementedException();
+    }
+}
